51247004d505401cdf65b7f8db08c1bb8bb9ce35
MukendiMputu <mputusam@live.com>
2020-01-21 11:21:25 -0600

    initializing learning path
    Signed-off-by: MukendiMputu <mputusam@live.com>

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("Hello, world!")
+name = input("What's your name? ")
+
+print("Nice to meet you,", name)
+answer = input("Are you enjoying learning python?")
+
+if answer:
+    print("Awesome!")
+else:
+    print("Oh ok. You should think about alternatives then!")
+
+def say_hello():
+    print("Hello!")
+
+say_hello()
